ea7aefe7accbeb0ed40eefeb78437375b13412b7
26msdev <124667980+26msdev@users.noreply.github.com>
2023-02-28 00:33:55 +0530

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chatgpt.fasttools.online', {
+  const response = await fetch('https://chat-gpt-p87o.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
